978e72a787a6e4fc742050b1a4d6c535a66b18cf
0x61696bb6f <luqmanbagas24@gmail.com>
2025-06-10 14:47:14 +0700

    Refactor docker-compose and database connection settings; switch to env_file for environment variables and update database credentials

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # URL koneksi ke database MySQL menggunakan driver pymysql
-DB_URL = "mysql+pymysql://root:@localhost/absensi_db"
+DB_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:@localhost/absensi_db")
 
 # Membuat engine SQLAlchemy untuk koneksi ke database
 engine = create_engine(DB_URL)
